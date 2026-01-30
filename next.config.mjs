8bb6146a8a57228d42fcbc8646ba21abbd56e691
hlwuser <newu46312@gmail.com>
2026-01-31 00:01:16 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: '/newsite',
+  assetPrefix: '/newsite',
 }
 
 export default nextConfig
